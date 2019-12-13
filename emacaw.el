257a99c2089fda292c0072a8635e494aa6f5a361
ibrown <ibrown@twitter.com>
2019-12-12 22:55:24 -0800

    minor refactor

--- emacaw.el
+++ emacaw.el
@@ -5,10 +5,12 @@
   "Send a tweet."
   (interactive)
   (if (<= (buffer-size) 140)
-      (print 
-       (buffer-substring-no-properties 1 (buffer-size)))
+      (send-tweet (buffer-substring-no-properties 1 (buffer-size)))
     (print "MORE THAN 140 CHARS!"))
    )
   
 
-(defun send-tweet (tweet-body)) 
+(defun send-tweet (tweet-body)
+  (print tweet-body)
+
+) 
