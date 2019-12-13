2e3647a53f8707dc6adcdaa4514864a16421325c
ibrown <ibrown@twitter.com>
2019-12-12 23:10:32 -0800

    added pbcopy impl

--- emacaw.el
+++ emacaw.el
@@ -5,12 +5,15 @@
   "Send a tweet."
   (interactive)
   (if (<= (buffer-size) 140)
-      (send-tweet (buffer-substring-no-properties 1 (buffer-size)))
+      (send-tweet 
+       (buffer-substring-no-properties 1 (buffer-size))
+       )
     (print "MORE THAN 140 CHARS!"))
    )
   
 
 (defun send-tweet (tweet-body)
   (print tweet-body)
+  (shell-command (concat "echo \"" tweet-body "\" | pbcopy"))
 
 ) 
