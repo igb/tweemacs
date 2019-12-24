f6dddf8b5cbd0874713a7ff5f4f0fa4a4bca338f
ibrown <ibrown@twitter.com>
2019-12-24 06:14:13 -0800

    create-parameter-string working-ish

--- emacaw.el
+++ emacaw.el
@@ -2,7 +2,7 @@
 (size-indication-mode t)
 
 (defun tweet ()
-  "Send a tweet."
+  "Send a tweet!"
   (interactive)
   (if (<= (buffer-size) 140)
       (send-tweet 
@@ -11,9 +11,45 @@
     (print "MORE THAN 140 CHARS!"))
    )
   
+(defun keep-output (process output)
+  (print output))
 
 (defun send-tweet (tweet-body)
-  (print tweet-body)
-  (shell-command (concat "echo \"" tweet-body "\" | pbcopy"))
+  (setq conn (open-network-stream "tweet" nil  "127.0.0.1" 8000))
+  (set-process-filter conn 'keep-output)
+  (process-send-string conn "GET /library.csv\n\n")
 
-) 
+ 
+  )
+
+;TODO: implement this
+(defun escape-uri (x) x)
+
+
+
+(defun encode-parameters (parameters)
+  (mapcar
+   (lambda (parameter)
+     	(cons (escape-uri (car parameter)) (escape-uri (cdr  parameter))))  parameters))
+
+
+
+(defun create-parameter-string (parameters)
+  (setq encoded-parameters  (encode-parameters parameters))
+  (setq sorted-encoded-parameters
+	(sort encoded-parameters
+	      (lambda (a b)
+		(string< (car a) (car b)))))
+
+  (mapconcat (lambda (y) (concat (car y) "=" (cdr y)))  sorted-encoded-parameters  "&"))
+  
+; (defun sign (parameters  url  consumer-secret  oauth-token-secret  http-method)
+;  parameter-string = (create-parameter-string parameters)
+;  signature-base-string = create-signature-base-string(parameter-string, url, http-method)
+;  signing-key= get-signing-key(consumer-secret, oauth-token-secret)
+;      base64-encode-to-string(crypto-hmac(sha, signing-key, signature-base-string))
+
+
+
+
+	
